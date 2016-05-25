ce5a1f3a31d038b4b23cfeca10c19e00e370c096
TheQ-v1 <mnazanar@gmail.com>
2016-05-25 16:39:59 +0300

    add meal controller, add draft views

---
+++ OnlineDietManager.WebUI/Models/MealViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OnlineDietManager.Domain.CoursesManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineDietManager.WebUI.Models
+{
+    public class MealViewModel
+    {
+        public Meal Meal { get; set; }
+        public string returnUrl { get; set; }
+        public int Day_ID { get; set; }
+    }
+}
